20f20c538fb3cf746b435422c9423045073e0303
Abel Lucas <abelucas009@gmail.com>
2023-10-08 22:52:40 -0300

    Reajustes com CustomTkinter
    Trocando Tkinter por CustomTkinter
    
    -frame centralizado(mesmo se redimensionar)
    
    -Conversor funcionando corretamente

---
+++ QRcode_Bit.py
@@ -0,0 +1,57 @@
+############
+#QRcode Bit#
+############
+from customtkinter import *
+from os.path import dirname
+from PIL import Image
+
+class Master(CTk):
+    def __init__(self, width= int, height= int, title= str, resizable= bool):
+        super().__init__()
+        self.minsize(width,height)
+        self.title(title)
+        self.resizable(resizable,resizable)
+        self.iconbitmap(dirname(__file__) +"\\resources/icon.ico")
+        set_appearance_mode("Light")#"dark"
+
+        self.rowconfigure(7, weight=6)
+        self.columnconfigure(3, weight=3)
+
+class Frame(CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.lbl1= CTkLabel(self, text="QRcode bit", font=("segoe ui black", 15))
+        self.lbl1.grid(row=2,column=2,columnspan=3)
+        
+        self.lbl2= CTkLabel(self, text="Link:", font=("segoe ui semibold", 15))
+        self.lbl2.grid(row=3,column=2,columnspan=3)
+
+        self.linkEntry= CTkEntry(self)
+        self.linkEntry.grid(row=4,column=2,columnspan=3)
+
+        self.btnOk= CTkButton(self, text="Converter", command= self.showQrcode)
+        self.btnOk.grid(row=5, column=2,columnspan=3)
+
+    def createQRcode(self):
+        import pyqrcode
+        link = str(self.linkEntry.get())
+        url= pyqrcode.create(link)
+        img= url.png('Qrcode.png', scale= 4)
+        
+    def showQrcode(self):
+        self.createQRcode()
+        self.img =CTkImage(light_image=Image.open(dirname(__file__) +'/Qrcode.png'), size=(250,250))
+        self.imglbl= CTkLabel(self, image= self.img, text= None)
+        self.imglbl.grid(row=6, column=2,columnspan=3)
+
+class App:
+    def __init__(self):
+        self.master= Master(350,450,"Qrcode bit",True)
+        self.frame= Frame(self.master)
+
+        self.frame.grid(row=2,column=2,rowspan=7,columnspan=3)
+        self.master.mainloop()
+
+if __name__ =="__main__":
+    App()
